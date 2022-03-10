4ecbac8248c5ed76be45761ce9645f8d7fb433f7
Michał Kolczok <kolczok1337@gmail.com>
2022-03-10 20:09:10 +0100

    Firebase part 1

--- app/src/main/java/com/judi/fitappka/MainActivity.kt
+++ app/src/main/java/com/judi/fitappka/MainActivity.kt
@@ -1,11 +1,45 @@
 package com.judi.fitappka
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        // Configure sign-in to request the user's ID, email address, and basic
+// profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestEmail()
+            .build()
+
+        // Build a GoogleSignInClient with the options specified by gso.
+        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+
+        //Runs when Log In button clicked
+        findViewById<Button>(R.id.buttonLogIn).setOnClickListener{
+            findViewById<TextView>(R.id.cos).text = "zalogowano"
+            val signInIntent = mGoogleSignInClient.signInIntent
+            registerForActivityResult(signInIntent, RC_SIGN_IN)
+        }
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        // Check for existing Google Sign In account, if the user is already signed in
+        // the GoogleSignInAccount will be non-null.
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+
+        // if NULL show log in button else hide it
+        //updateUI(account)
     }
 }
