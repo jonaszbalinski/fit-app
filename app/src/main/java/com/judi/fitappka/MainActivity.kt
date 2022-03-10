973edb13737c67edc9529c167011d81684e805e4
Jonasz Balinski <->
2022-03-10 22:47:55 +0100

    Firebase part 2

--- app/src/main/java/com/judi/fitappka/MainActivity.kt
+++ app/src/main/java/com/judi/fitappka/MainActivity.kt
@@ -2,6 +2,7 @@ package com.judi.fitappka
 
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import android.widget.Button
 import android.widget.TextView
@@ -19,84 +20,27 @@ import com.google.firebase.auth.FirebaseAuth
 
 
 class MainActivity : AppCompatActivity() {
-    private val signInLauncher = registerForActivityResult(
-        FirebaseAuthUIActivityResultContract()
-    ) { res ->
-        this.onSignInResult(res)
-    }
+    private lateinit var mAuth: FirebaseAuth
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        // Configure sign-in to request the user's ID, email address, and basic
-        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
-        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestEmail()
-            .build()
+        mAuth = FirebaseAuth.getInstance()
+        val user = mAuth.currentUser
 
-        val mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
-
-        //Runs when Log In button clicked
-        findViewById<Button>(R.id.buttonLogIn).setOnClickListener{
-            findViewById<TextView>(R.id.cos).text = "zalogowano"
-        }
-
-    }
-
-    override fun onStart() {
-        super.onStart()
-        // Check for existing Google Sign In account, if the user is already signed in
-        // the GoogleSignInAccount will be non-null.
-        val account = GoogleSignIn.getLastSignedInAccount(this)
-
-        // if NULL show log in button else hide it
-        //updateUI(account)
-    }
-
-    private fun createSignInIntent() {
-        // [START auth_fui_create_intent]
-        // Choose authentication providers
-        val providers = arrayListOf(
-            AuthUI.IdpConfig.EmailBuilder().build(),
-            AuthUI.IdpConfig.PhoneBuilder().build(),
-            AuthUI.IdpConfig.GoogleBuilder().build(),
-            AuthUI.IdpConfig.FacebookBuilder().build(),
-            AuthUI.IdpConfig.TwitterBuilder().build())
-
-        // Create and launch sign-in intent
-        val signInIntent = AuthUI.getInstance()
-            .createSignInIntentBuilder()
-            .setAvailableProviders(providers)
-            .build()
-        signInLauncher.launch(signInIntent)
-        // [END auth_fui_create_intent]
-    }
-
-    private fun onSignInResult(result: FirebaseAuthUIAuthenticationResult) {
-        val response = result.idpResponse
-        if (result.resultCode == RESULT_OK) {
-            // Successfully signed in
-            val user = FirebaseAuth.getInstance().currentUser
-            // ...
-        } else {
-            // Sign in failed. If response is null the user canceled the
-            // sign-in flow using the back button. Otherwise check
-            // response.getError().getErrorCode() and handle the error.
-            // ...
-        }
-    }
-
-    private fun signOut() {
-        // [START auth_fui_signout]
-        AuthUI.getInstance()
-            .signOut(this)
-            .addOnCompleteListener {
-                // ...
+        Handler().postDelayed({
+            if (user != null) {
+                //val loginIntent = Intent(this, LoginActivity::class.java)
+                //startActivity(loginIntent)
+                finish()
             }
-        // [END auth_fui_signout]
+            else {
+                val singInIntent = Intent(this, LoginActivity::class.java)
+                startActivity(singInIntent)
+                finish()
+            }
+        }, 2000)
     }
 }
