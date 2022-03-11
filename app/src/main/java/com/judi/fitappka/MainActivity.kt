b9bb0c952c0bc6b40b58eafa86e3e2cf00c69b4d
Jonasz Balinski <->
2022-03-11 09:31:08 +0100

    Change auth method to e-mail/password

--- app/src/main/java/com/judi/fitappka/MainActivity.kt
+++ app/src/main/java/com/judi/fitappka/MainActivity.kt
@@ -1,46 +1,22 @@
 package com.judi.fitappka
 
 import android.content.Intent
-import android.os.Bundle
-import android.os.Handler
-import android.util.Log
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.AuthUI.TAG
-import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
-import com.firebase.ui.auth.data.model.FirebaseAuthUIAuthenticationResult
-import com.google.android.gms.auth.api.signin.GoogleSignIn
-import com.google.android.gms.auth.api.signin.GoogleSignInAccount
-import com.google.android.gms.auth.api.signin.GoogleSignInOptions
-import com.google.android.gms.common.api.ApiException
-import com.google.android.gms.tasks.Task
-import com.google.firebase.auth.FirebaseAuth
-
+import android.os.Bundle
+import extensions.Extensions.toast
+import utils.FirebaseUtils.firebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var mAuth: FirebaseAuth
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mAuth = FirebaseAuth.getInstance()
-        val user = mAuth.currentUser
-
-        Handler().postDelayed({
-            if (user != null) {
-                //val loginIntent = Intent(this, LoginActivity::class.java)
-                //startActivity(loginIntent)
-                finish()
-            }
-            else {
-                val singInIntent = Intent(this, LoginActivity::class.java)
-                startActivity(singInIntent)
-                finish()
-            }
-        }, 2000)
+        btnSignOut.setOnClickListener {
+            firebaseAuth.signOut()
+            startActivity(Intent(this, CreateAccountActivity::class.java))
+            toast("signed out")
+            finish()
+        }
     }
 }
---
+++ app/src/main/java/com/judi/fitappka/SignInActivity.kt
@@ -0,0 +1,57 @@
+package com.judi.fitappka
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
+import extensions.Extensions.toast
+/** fix missing imports **/
+import kotlinx.android.synthetic.main.activity_sign_in.*
+import utils.FirebaseUtils.firebaseAuth
+
+class SignInActivity : AppCompatActivity() {
+    lateinit var signInEmail: String
+    lateinit var signInPassword: String
+    lateinit var signInInputsArray: Array<EditText>
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+
+        signInInputsArray = arrayOf(etSignInEmail, etSignInPassword)
+        btnCreateAccount2.setOnClickListener {
+            startActivity(Intent(this, CreateAccountActivity::class.java))
+            finish()
+        }
+
+        btnSignIn.setOnClickListener {
+            signInUser()
+        }
+    }
+
+    private fun notEmpty(): Boolean = signInEmail.isNotEmpty() && signInPassword.isNotEmpty()
+
+    private fun signInUser() {
+        signInEmail = etSignInEmail.text.toString().trim()
+        signInPassword = etSignInPassword.text.toString().trim()
+
+        if (notEmpty()) {
+            firebaseAuth.signInWithEmailAndPassword(signInEmail, signInPassword)
+                .addOnCompleteListener { signIn ->
+                    if (signIn.isSuccessful) {
+                        startActivity(Intent(this, MainActivity::class.java))
+                        toast("signed in successfully")
+                        finish()
+                    } else {
+                        toast("sign in failed")
+                    }
+                }
+        } else {
+            signInInputsArray.forEach { input ->
+                if (input.text.toString().trim().isEmpty()) {
+                    input.error = "${input.hint} is required"
+                }
+            }
+        }
+    }
+}
