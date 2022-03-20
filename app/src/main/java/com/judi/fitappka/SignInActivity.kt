8e13f4a87f8211e31e694271ab1a0e3b48b030ba
Jonasz Balinski <->
2022-03-20 12:39:59 +0100

    Add authentication by Google

--- app/src/main/java/com/judi/fitappka/SignInActivity.kt
+++ app/src/main/java/com/judi/fitappka/SignInActivity.kt
@@ -2,16 +2,30 @@ package com.judi.fitappka
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 import com.judi.fitappka.databinding.ActivitySignInBinding
 import extensions.Extensions.toast
 import utils.FirebaseUtils.firebaseAuth
+
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var signInEmail: String
     private lateinit var signInPassword: String
     private lateinit var signInInputsArray: Array<EditText>
+
+    private lateinit var mGoogleSignInClient: GoogleSignInClient
+    private lateinit var auth: FirebaseAuth
 
     private lateinit var binding: ActivitySignInBinding
 
@@ -20,6 +34,14 @@ class SignInActivity : AppCompatActivity() {
         binding = ActivitySignInBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+
+        mGoogleSignInClient = GoogleSignIn.getClient(this, gso)
+        auth = Firebase.auth
+
         signInInputsArray = arrayOf(binding.etSignInEmail, binding.etSignInPassword)
         binding.btnCreateAccount2.setOnClickListener {
             startActivity(Intent(this, CreateAccountActivity::class.java))
@@ -27,13 +49,68 @@ class SignInActivity : AppCompatActivity() {
         }
 
         binding.btnSignIn.setOnClickListener {
-            signInUser()
+            signInEmailUser()
+        }
+
+        binding.buttonGoogleSignIn.setOnClickListener {
+            signInGoogleUser()
+        }
+
+        for (i in 0 until binding.buttonGoogleSignIn.childCount) {
+            val v: View = binding.buttonGoogleSignIn.getChildAt(i)
+            if (v is TextView) {
+                v.text = getString(R.string.sign_in_with_google)
+                return
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == 1) {
+            val task = GoogleSignIn.getSignedInAccountFromIntent(data)
+            try {
+                val account = task.getResult(ApiException::class.java)!!
+                toast("firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                toast(e.toString())
+            }
+        }
+    }
+
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    toast("signInWithCredential:success")
+                } else {
+                    toast("signInWithCredential:failure")
+                    // val view = binding.mainLayout
+                }
+            }
+    }
+
+    private fun signInGoogleUser() {
+        val signInIntent = mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent, 1)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val account = GoogleSignIn.getLastSignedInAccount(this)
+        if (account != null){
+            toast("Get last logged-in Google account!")
+        }
+        else {
+            toast("No Google account logged-in")
         }
     }
 
     private fun notEmpty(): Boolean = signInEmail.isNotEmpty() && signInPassword.isNotEmpty()
 
-    private fun signInUser() {
+    private fun signInEmailUser() {
         signInEmail = binding.etSignInEmail.text.toString().trim()
         signInPassword = binding.etSignInPassword.text.toString().trim()
 
