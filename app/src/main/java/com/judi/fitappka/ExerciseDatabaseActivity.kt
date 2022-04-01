755ef333b0339ac0011a977390329bac96ebcef0
Jonasz Balinski <->
2022-04-01 11:36:53 +0200

    Add basic exercise info view

--- app/src/main/java/com/judi/fitappka/ExerciseDatabaseActivity.kt
+++ app/src/main/java/com/judi/fitappka/ExerciseDatabaseActivity.kt
@@ -1,13 +1,20 @@
 package com.judi.fitappka
 
+import android.content.Context
 import android.os.Bundle
 import android.view.Gravity
+import android.view.View
+import android.widget.Button
 import android.widget.CalendarView.OnDateChangeListener
+import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.get
+import androidx.core.view.isEmpty
 import com.google.firebase.database.*
 import com.judi.fitappka.databinding.ActivityExerciseDatabaseBinding
 import extensions.Extensions.toast
+import org.w3c.dom.Text
 
 
 class ExerciseDatabaseActivity : AppCompatActivity() {
@@ -57,34 +64,20 @@ class ExerciseDatabaseActivity : AppCompatActivity() {
             }
         })
 
-        binding.calendarView.setOnDateChangeListener(OnDateChangeListener { _, year, month, day ->
+        binding.calendarView.setOnDateChangeListener { _, year, month, day ->
             selectedDate = ""
-            selectedDate += if(day < 10) "0$day" else "$day"
+            selectedDate += if (day < 10) "0$day" else "$day"
             val fixedMonth = month + 1
-            selectedDate += if(fixedMonth < 10) "0$fixedMonth" else "$fixedMonth"
+            selectedDate += if (fixedMonth < 10) "0$fixedMonth" else "$fixedMonth"
             selectedDate += "$year"
-
-            binding.linearLayout.removeAllViews()
-            val tv = TextView(this)
-            tv.text = selectedDate
-            tv.textSize += 1
-            tv.gravity = Gravity.CENTER
-            binding.linearLayout.addView(tv)
-            binding.linearLayout.addView(TextView(this))
 
             exerciseDataReference.get().addOnSuccessListener {
                 updateExerciseList(it)
-                for(exercise in exerciseDataSet) {
-                    val exerciseTV = TextView(this)
-                    exerciseTV.text = exercise.getValues()
-                    exerciseTV.gravity = Gravity.CENTER
-                    binding.linearLayout.addView(exerciseTV)
-                }
-            }.addOnFailureListener{
+            }.addOnFailureListener {
                 toast("Error during receiving data")
             }
 
-        })
+        }
     }
 
     private fun updateExerciseList(snapshot: DataSnapshot) {
@@ -100,5 +93,87 @@ class ExerciseDatabaseActivity : AppCompatActivity() {
                 }
             }
         }
+        updateExerciseView()
+    }
+
+    private fun updateExerciseView() {
+        binding.scrollViewButtons.removeAllViews()
+        binding.scrollViewExercise.removeAllViews()
+        val ll = LinearLayout(this)
+        ll.orientation = LinearLayout.HORIZONTAL
+        binding.scrollViewButtons.addView(ll)
+
+        ll.gravity = Gravity.CENTER
+
+        for (exercise in exerciseDataSet) {
+            val button = Button(this)
+            button.text = exercise.id.toString()
+            ll.addView(button)
+            openExerciseInfo(exercise)
+        }
+    }
+
+    private fun openExerciseInfo(exercise: Exercise) {
+        binding.scrollViewExercise.removeAllViews()
+        val ll = LinearLayout(this)
+        ll.orientation = LinearLayout.VERTICAL
+        binding.scrollViewExercise.addView(ll)
+        ll.gravity = Gravity.CENTER
+
+        // title
+        val title = TextView(this)
+        title.text = exercise.name
+        ll.addView(title)
+
+        // data
+        if(exercise.series != null) {
+            addPropertyInfoToExercise(ll, getString(R.string.series),
+                exercise.series.toString())
+        }
+        if(exercise.reps != null) {
+            addPropertyInfoToExercise(ll, getString(R.string.reps),
+                exercise.reps.toString())
+        }
+        if(exercise.weight != null) {
+            addPropertyInfoToExercise(ll, getString(R.string.weight),
+                exercise.weight.toString())
+        }
+        if(exercise.distance != null) {
+            addPropertyInfoToExercise(ll, getString(R.string.distance),
+                exercise.distance.toString())
+        }
+        if(exercise.duration != null) {
+            addPropertyInfoToExercise(ll, getString(R.string.duration),
+                exercise.duration.toString())
+        }
+
+        // buttons
+        val horizontalLL = LinearLayout(this)
+        horizontalLL.orientation = LinearLayout.HORIZONTAL
+
+        val buttonDelete = Button(this)
+        buttonDelete.text = getString(R.string.delete_exercise)
+
+        val buttonEdit = Button(this)
+        buttonEdit.text = getString(R.string.edit_exercise)
+
+        horizontalLL.addView(buttonDelete)
+        horizontalLL.addView(buttonEdit)
+        ll.addView(horizontalLL)
+    }
+
+    private fun addPropertyInfoToExercise(parent: LinearLayout, propertyName: String, value: String) {
+        val horizontalLL = LinearLayout(this)
+
+        horizontalLL.orientation = LinearLayout.HORIZONTAL
+        val propertyTextView = TextView(this)
+        propertyTextView.text = propertyName
+
+        val valueTextView = TextView(this)
+        valueTextView.text = value
+
+        horizontalLL.addView(propertyTextView)
+        horizontalLL.addView(valueTextView)
+        parent.addView(horizontalLL)
     }
 }
